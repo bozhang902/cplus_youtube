78c15c3c9a11d3f098a2307b3bd124a7f548815d
Bob <108517101+by902@users.noreply.github.com>
2023-09-05 22:07:10 +0800

    Update first_cpp.cpp

--- Chapter1/first_cpp.cpp
+++ Chapter1/first_cpp.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+int main()
+{
+	using namespace std;
+	cout << "Comp up and C++ me some time";
+	cout << endl;
+	cout << "You won't  regret it!";
+	cout << endl;
+
+	return 0;
+}
